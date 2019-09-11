5337091fd7b3455293521209dcbfc2e996630959
陈宣润 <815135619@qq.com>
2019-09-11 22:38:43 +0800

    cxr[update]依赖更新

--- src/main/java/com/mmall/MmallApplication.java
+++ src/main/java/com/mmall/MmallApplication.java
@@ -3,13 +3,19 @@ package com.mmall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.mmall.dao") // mybatis扫描路径，针对的是接口Mapper类
-public class MmallApplication {
+public class MmallApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MmallApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(MmallApplication.class);
+    }
 }
