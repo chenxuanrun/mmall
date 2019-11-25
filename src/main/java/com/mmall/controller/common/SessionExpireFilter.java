51fcd83c6e7c4a72f71f0ac487c992018de7f482
chenxuanrun <815135619@qq.com>
2019-11-25 10:17:24 +0800

    日常更新

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,52 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @program: mmall
+ * @description:
+ * @author: cxr
+ * @create: 2019-11-25 10:05
+ **/
+@Component
+@WebFilter(urlPatterns = "*.do",filterName = "sessionExpireFilter")
+public class SessionExpireFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtils.isNotBlank(loginToken)){
+            //判断logintoken是否为空或者"";
+            //如果不为空,符合条件,继续拿user信息
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJsonStr,User.class);
+            if(Objects.nonNull(user)){
+                //如果user不为空,则重置session的时间,即调用expire命令
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+            chain.doFilter(request,response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
